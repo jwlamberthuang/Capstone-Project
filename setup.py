96440c889753c7e267c043ba8e50b11735fb00dc
Lambert Huang <104709535+jwlamberthuang@users.noreply.github.com>
2022-07-23 07:11:13 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+  name="RandomModel",
+  author="Jingwei Huang",
+  author_email="huangjingwei1123@gmail.com",
+  description="This package provides AI-driven solutions.",
+  packages=["DeepLearning"]
+)
